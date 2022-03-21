6e45d6ff210a876a818fda78a4c15ca7859667c8
XIO <qq1954556630@gmail.com>
2022-03-21 16:45:51 +0800

    Model test OK !

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Models\Product;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::get('/products', function() {
+//     return 'products';
+// });
+
+Route::get('/products', function() {
+    return Product::all();
+});
+
+Route::post('/products', function() {
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'descripyion' => 'this is a product one',
+        'price' => '99.99'
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
